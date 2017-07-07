4b763aba899c74f865c14762ddbba4163d998b0d
Zygimantus <zygimantus@gmail.com>
2017-07-07 22:44:54 +0300

    Cards class for constants

---
+++ src/main/java/com/deckofcards/Cards.java
@@ -0,0 +1,64 @@
+package com.deckofcards;
+
+/**
+ *
+ * @author Zygimantus
+ */
+public class Cards {
+
+    public static final String SA = "AS";
+    public static final String S2 = "2S";
+    public static final String S3 = "3S";
+    public static final String S4 = "4S";
+    public static final String S5 = "5S";
+    public static final String S6 = "6S";
+    public static final String S7 = "7S";
+    public static final String S8 = "8S";
+    public static final String S9 = "9S";
+    public static final String S0 = "0S";
+    public static final String SJ = "JS";
+    public static final String SQ = "QS";
+    public static final String SK = "KS";
+
+    public static final String CA = "AC";
+    public static final String C2 = "2C";
+    public static final String C3 = "3C";
+    public static final String C4 = "4C";
+    public static final String C5 = "5C";
+    public static final String C6 = "6C";
+    public static final String C7 = "7C";
+    public static final String C8 = "8C";
+    public static final String C9 = "9C";
+    public static final String C0 = "0C";
+    public static final String CJ = "JC";
+    public static final String CQ = "QC";
+    public static final String CK = "KC";
+
+    public static final String HA = "AH";
+    public static final String H2 = "2H";
+    public static final String H3 = "3H";
+    public static final String H4 = "4H";
+    public static final String H5 = "5H";
+    public static final String H6 = "6H";
+    public static final String H7 = "7H";
+    public static final String H8 = "8H";
+    public static final String H9 = "9H";
+    public static final String H0 = "0H";
+    public static final String HJ = "JH";
+    public static final String HQ = "QH";
+    public static final String HK = "KH";
+
+    public static final String DA = "AD";
+    public static final String D2 = "2D";
+    public static final String D3 = "3D";
+    public static final String D4 = "4D";
+    public static final String D5 = "5D";
+    public static final String D6 = "6D";
+    public static final String D7 = "7D";
+    public static final String D8 = "8D";
+    public static final String D9 = "9D";
+    public static final String D0 = "0D";
+    public static final String DJ = "JD";
+    public static final String DQ = "QD";
+    public static final String DK = "KD";
+}
